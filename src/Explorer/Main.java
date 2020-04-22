c5eb311130e086a79807113d1ba6d441d9a39d6a
Korjick <zzgriim@gmail.com>
2020-04-22 13:59:52 +0300

    // 22.04.2020

---
+++ src/Explorer/Main.java
@@ -0,0 +1,67 @@
+package Explorer;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    static String text;
+    static Scanner in = new Scanner(System.in);
+    static Path path = Paths.get(".").normalize().toAbsolutePath();
+
+    public static void main(String[] args) {
+        System.out.print("<" + path + ">: ");
+
+        while (!(text = in.nextLine()).equals("quit")) {
+            parser(text);
+            System.out.print("<" + path + ">: ");
+        }
+    }
+
+    public static void parser(String text) {
+        try {
+            if (text.length() > 2) {
+                if (text.trim().toLowerCase().substring(0,2).equals("cd")) {
+                    text = text.substring(2).trim();
+
+                    if (text.equals("..")) {
+                        if (!path.equals(path.getRoot())) path = path.getParent();
+                        return;
+                    }
+
+                    if(text.contains("/")){
+                        Path abs = Paths.get(text);
+                        if(Files.exists(abs)) path = Paths.get(abs.toString());
+                    } else{
+                        path = Paths.get(path.toString() + "/" + text).normalize().toAbsolutePath();
+                        if (!Files.exists(path)) path = path.getParent();
+                    }
+                }
+
+                if(text.trim().toLowerCase().substring(0,3).equals("dir")){
+                    File fileSystem = new File(path.toString());
+                    System.out.println("[ <--------");
+                    for(File file : fileSystem.listFiles()) System.out.println(file);
+                    System.out.println("--------> ]");
+                }
+
+                if(text.trim().toLowerCase().substring(0,4).equals("open")){
+                    text = text.substring(4).trim();
+                    Path run = Paths.get(path.toString() + "/" + text);
+                    System.out.println(run);
+                    if(Files.exists(run) && !Files.isDirectory(run)){
+                        Process process = new ProcessBuilder(path.toString()).start();
+                    }
+                }
+            }
+        } catch (InvalidPathException e) {
+            System.out.println("Путь введен некорректно");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
