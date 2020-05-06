b39e8187b345b44cd17d250b6ada3f028208fd03
Korjick <zzgriim@gmail.com>
2020-05-06 17:40:43 +0300

    // 05.05.2020

--- src/Explorer/Main.java
+++ src/Explorer/Main.java
@@ -5,8 +5,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -14,12 +18,31 @@ public class Main {
     static Scanner in = new Scanner(System.in);
     static Path path = Paths.get(".").normalize().toAbsolutePath();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.print("<" + path + ">: ");
         while (!(text = in.nextLine()).toLowerCase().trim().equals("quit")) {
             parser(text.toLowerCase().trim());
             System.out.print("<" + path + ">: ");
         }
+    }
+
+    public static void recursiveDelete(Path delete) throws IOException {
+        if (Files.isDirectory(delete))
+            for (File f : Objects.requireNonNull(delete.toFile().listFiles()))
+                recursiveDelete(delete);
+
+        Files.delete(delete);
+    }
+
+    public static void recursiveCopy(Path from, Path to) throws IOException {
+        if (Files.isDirectory(from)) {
+            Files.copy(from, to);
+            for (File f : Objects.requireNonNull(from.toFile().listFiles()))
+                recursiveCopy(f.toPath().toAbsolutePath().normalize(),
+                        to.resolve(f.getName()));
+        }
+
+        Files.copy(from, to);
     }
 
     public static void parser(String text) {
@@ -29,12 +52,13 @@ public class Main {
 
             if (cmd.equals("cd")) {
                 if (line.length > 1) {
-                    if (line[1].equals("..") && !path.equals(path.getRoot())) {
+                    String param = line[1].trim().toLowerCase();
+                    if (param.equals("..") && !path.equals(path.getRoot())) {
                         path = path.getParent();
                         return;
                     }
 
-                    Path absolute = path.resolve(line[1]).toAbsolutePath().normalize();
+                    Path absolute = path.resolve(param).toAbsolutePath().normalize();
                     if (Files.exists(absolute) && Files.isDirectory(absolute)) path = Paths.get(absolute.toString());
                 } else {
                     System.out.println("Введите путь к папке");
@@ -63,7 +87,8 @@ public class Main {
 
             if (cmd.equals("run")) {
                 if (line.length > 1) {
-                    Path run = path.resolve(line[1]).toAbsolutePath().normalize();
+                    String param = line[1].trim().toLowerCase();
+                    Path run = path.resolve(param).toAbsolutePath().normalize();
 
                     if (Files.exists(run) && !Files.isDirectory(run)) {
                         System.out.println("Запуск программы...");
@@ -76,14 +101,28 @@ public class Main {
 
             if (cmd.equals("copy")) {
                 if (line.length > 1) {
-                    String[] fromTo = line[1].split("", 2);
-                    if (fromTo.length > 1) {
-                        Path from = path.resolve(fromTo[0]).toAbsolutePath().normalize();
-                        Path to = path.resolve(fromTo[1]).toAbsolutePath().normalize();
+                    ArrayList<String> fromTo = new ArrayList<>();
+                    String param = line[1].trim().toLowerCase();
+
+                    if (param.contains("\"")) {
+                        Pattern pattern = Pattern.compile("\"([a-zA-Z\\s0-9/\\\\.]+)\"");
+                        Matcher matcher = pattern.matcher(param);
+
+                        while (matcher.find()) {
+                            String add = matcher.group();
+                            add = add.substring(add.indexOf("\"") + 1, add.lastIndexOf("\""));
+                            fromTo.add(add);
+                            System.out.println(add);
+                        }
+                    } else fromTo.addAll(Arrays.asList(line[1].trim().split(" ", 2)));
+
+                    if (fromTo.size() > 1) {
+                        Path from = path.resolve(fromTo.get(0).toLowerCase().trim()).toAbsolutePath().normalize();
+                        Path to = path.resolve(fromTo.get(1).toLowerCase().trim()).toAbsolutePath().normalize();
 
                         if (Files.exists(from) && Files.exists(to.getParent())) {
                             System.out.println("Копирование файла...");
-                            Files.copy(from, to);
+                            recursiveCopy(from, to);
                         }
                     }
                 } else {
@@ -94,11 +133,12 @@ public class Main {
 
             if (cmd.equals("delete")) {
                 if (line.length > 1) {
-                    Path delete = path.resolve(line[1]).toAbsolutePath().normalize();
+                    String param = line[1].trim().toLowerCase();
+                    Path delete = path.resolve(param).toAbsolutePath().normalize();
 
                     if (Files.exists(delete)) {
                         System.out.println("Удаление файла...");
-                        Files.delete(delete);
+                        recursiveDelete(delete);
                     }
                 } else {
                     System.out.println("Введите путь к файлу для удаления");
